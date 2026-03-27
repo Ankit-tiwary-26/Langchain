b59b7788de2dcdd27d2a654f1bbd885c4dc2ad0f
Ankit Raj Tiwary <tiwaryankit26@gmail.com>
2026-03-27 14:48:41 +0530

    Update API key to placeholder in 1_llm_demo.py
    Replaced the Google API key with a placeholder.

--- 1_llm_demo.py
+++ 1_llm_demo.py
@@ -2,7 +2,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.0-flash",   
-    google_api_key="AIzaSyD5C-ebqeUv8OVsMmXtVkaSyOn79SgA7Ig"
+    google_api_key="Api_key"
 )
 
 print(llm.invoke("What is the capital of India?").content)
